e29c0effd37dcc091d45e83dc42a4e25aafd5ea5
Shuo Jiang <34135398+jiangshuozz@users.noreply.github.com>
2023-05-23 00:54:31 +0800

    Update one-key-deploy.sh
    部署状态查询脚本

--- one-key-deploy.sh
+++ one-key-deploy.sh
@@ -3,9 +3,13 @@
 # 替换温控脚本文件
 cp ./etc/init.d/fa-rk3328-pwmfan /etc/init.d/fa-rk3328-pwmfan
 cp ./usr/bin/start-rk3328-pwm-fan.sh /usr/bin/start-rk3328-pwm-fan.sh
+cp ./root/pwm-fan-status.sh /root/pwm-fan-status.sh
 # 脚本添加执行权限
 chmod +x /usr/bin/start-rk3328-pwm-fan.sh
 chmod +x /etc/init.d/fa-rk3328-pwmfan
+chmod +x /root/pwm-fan-status.sh
 # 启动温控服务
 /etc/init.d/fa-rk3328-pwmfan enable
 /etc/init.d/fa-rk3328-pwmfan start
+sleep 10s
+./pwm-fan-status.sh
